7a271c4ca38803baecbda80153b5c9a8f695da38
Naveen Prasad <naveenacad89@gmail.com>
2025-01-13 20:38:12 +0530

    netifly-file-added

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -5,7 +5,7 @@ import UpdateSettingsForm from "../features/settings/UpdateSettingsForm";
 function Settings() {
   return (
     <Row>
-      <Heading as="h1">Update hotel settings</Heading>;
+      <Heading as="h1">Update hotel settings</Heading>
       <UpdateSettingsForm />
     </Row>
   );
